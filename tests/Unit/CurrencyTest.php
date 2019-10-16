5d71c281756b76c6812ef413952bbdacde3f44e0
ElijahCoding <cuiyeqing960904@gmail.com>
2019-10-16 20:08:39 +0300

    currency tests

--- tests/Unit/CurrencyTest.php
+++ tests/Unit/CurrencyTest.php
@@ -3,18 +3,23 @@
 namespace Tests\Unit;
 
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\WithFaker;
-use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Services\CurrencyService;
 
 class CurrencyTest extends TestCase
 {
-    /**
-     * A basic unit test example.
-     *
-     * @return void
-     */
-    public function testExample()
+    public function test_convert_usd_to_eur()
     {
-        $this->assertTrue(true);
+        $amount_in_usd = 100;
+        $this->assertEquals(89, (new CurrencyService())->convert($amount_in_usd, 'usd', 'eur'));
+    }
+    public function test_convert_gbp_to_eur()
+    {
+        $amount_in_gbp = 100;
+        $this->assertEquals(0, (new CurrencyService())->convert($amount_in_gbp, 'gbp', 'eur'));
+    }
+    public function test_convert_usd_to_gbp()
+    {
+        $amount_in_usd = 100;
+        $this->assertEquals(0, (new CurrencyService())->convert($amount_in_usd, 'usd', 'gbp'));
     }
 }
