58f71dff23391250e7c778fff96f2178e1cd2a39
ElijahCoding <cuiyeqing960904@gmail.com>
2019-10-16 20:03:21 +0300

    products tests built

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UpdateProductRequest;
 use App\Product;
 use Illuminate\Http\Request;
 
@@ -18,4 +19,74 @@ class ProductController extends Controller
 
         return view('products.index', compact('products'));
     }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('products.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $product = Product::create($request->all());
+
+        if ($request->hasFile('photo')) {
+            $filename = $request->photo->getClientOriginalName();
+            $request->photo->storeAs('logos', $filename);
+            $product->update(['photo' => $filename]);
+        }
+
+        return redirect()->route('products.index');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  Product $product
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Product $product)
+    {
+        return view('products.edit', compact('product'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  UpdateProductRequest  $request
+     * @param  Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateProductRequest $request, Product $product)
+    {
+        $product->update($request->all());
+
+        return redirect()->route('products.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  Product  $product
+     * @return \Illuminate\Http\Response
+     *
+     * @throws \Exception
+     */
+    public function destroy(Product $product)
+    {
+        $product->delete();
+
+        return redirect()->route('products.index');
+    }
+
 }
