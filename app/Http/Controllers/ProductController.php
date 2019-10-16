55c9d7d732b67c11ae633f9f9660c559e67a335f
ElijahCoding <cuiyeqing960904@gmail.com>
2019-10-16 11:04:48 +0300

    basic tests structure

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $products = Product::paginate(10);
+
+        return view('products.index', compact('products'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,14 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function() {
+    return redirect()->route('products.index');
 });
+
+
+Route::middleware('auth')->group(function () {
+    Route::get('products', 'ProductController@index')->name('products.index');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
