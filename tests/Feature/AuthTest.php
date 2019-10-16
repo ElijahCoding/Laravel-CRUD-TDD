41c158b693461338eaef38aab11783825982ecbc
ElijahCoding <cuiyeqing960904@gmail.com>
2019-10-16 19:36:09 +0300

    testing Auth

--- tests/Feature/AuthTest.php
+++ tests/Feature/AuthTest.php
@@ -2,20 +2,36 @@
 
 namespace Tests\Feature;
 
+use App\User;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class AuthTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function testExample()
+    use RefreshDatabase;
+
+    public function test_login_redirects_successfully()
     {
-        $response = $this->get('/');
+        factory(User::class)->create([
+            'email' => 'admin@admin.com',
+            'password' => bcrypt('password123'),
+        ]);
+
+        $response = $this->post('/login', ['email' => 'admin@admin.com', 'password' => 'password123']);
+
+        $response->assertStatus(302);
+        $response->assertRedirect('/home');
+    }
+
+    public function test_authenticated_user_can_access_products_table()
+    {
+        $user = factory(User::class)->create([
+            'email' => 'admin@admin.com',
+            'password' => bcrypt('password123'),
+        ]);
+
+        $response = $this->actingAs($user)->get('/products/');
 
         $response->assertStatus(200);
     }
--- tests/Feature/ProductsTest.php
+++ tests/Feature/ProductsTest.php
@@ -3,17 +3,22 @@
 namespace Tests\Feature;
 
 use Tests\TestCase;
+use App\{User, Product};
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ProductsTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function testExample()
+    use RefreshDatabase;
+
+    private $user;
+
+    private function create_user($user)
+    {
+
+    }
+
+    public function test_homepage_contains_empty_products_table()
     {
         $response = $this->get('/');
 
